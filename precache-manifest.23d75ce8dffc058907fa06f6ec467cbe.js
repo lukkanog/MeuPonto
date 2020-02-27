c58a5ffc3955ab96508057f437ab35799d4f1524
Lucas Nogueira <lukanog@hotmail.com>
2020-02-27 08:50:56 -0300

    Updates

---
+++ precache-manifest.23d75ce8dffc058907fa06f6ec467cbe.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "37e1783192898ecee9e92be0b794afb4",
+    "url": "/MeuPonto/index.html"
+  },
+  {
+    "revision": "30b9f75d64fcc2e03a5a",
+    "url": "/MeuPonto/static/css/main.385d48aa.chunk.css"
+  },
+  {
+    "revision": "7f39dce0da515d5cc775",
+    "url": "/MeuPonto/static/js/2.d752ca4a.chunk.js"
+  },
+  {
+    "revision": "4d05604127059121c8545b8fa9a7e3f9",
+    "url": "/MeuPonto/static/js/2.d752ca4a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "30b9f75d64fcc2e03a5a",
+    "url": "/MeuPonto/static/js/main.b7a9896f.chunk.js"
+  },
+  {
+    "revision": "70aa33898af3d127e5be",
+    "url": "/MeuPonto/static/js/runtime-main.5df449b7.js"
+  },
+  {
+    "revision": "5f52fec57f61a79eababbd459c75936c",
+    "url": "/MeuPonto/static/media/LogoMeuPonto.5f52fec5.png"
+  },
+  {
+    "revision": "eff9bb25fdaa24556e1757485658d9a4",
+    "url": "/MeuPonto/static/media/accept-green-icon.eff9bb25.png"
+  },
+  {
+    "revision": "0354216e1027ef1a3fd1794bf455b2e5",
+    "url": "/MeuPonto/static/media/background-login.0354216e.png"
+  },
+  {
+    "revision": "6a2707992cbe3f77cb86d0262c919110",
+    "url": "/MeuPonto/static/media/decline-user-darkred-icon.6a270799.png"
+  }
+]);
